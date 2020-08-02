8fb8067c38a680e92bb7315b2c5a44b428786bcb
Anuradha4781 <68979402+Anuradha4781@users.noreply.github.com>
2020-08-02 13:34:06 +0530

    website

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> True_Mentor.com</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-sacale=1">
+
+                <style>
+            .body{
+                margin:0px;
+                padding:0px;
+            }
+             .logo{
+             float: left;
+             padding-top: 5px;
+             padding-bottom: 5px;
+            
+              }
+              
+            .banner_image{
+                background-image: url("img/rightpath7.jpg");
+                background-size: cover;
+                background-position: center center;
+                background-repeat: no-repeat;
+              
+                
+                width:100%;
+                padding-top:50px;
+                padding-bottom: 50px;
+                text-align: center;
+                color: #f8f8f8;
+            }
+            .banner_content{
+                position: relative;
+                padding-top: 6%;
+                padding-bottom: 6%;
+                margin-top: 12%;
+                margin-bottom: 12%;
+                margin: 0 auto;
+                background-color: rgba(0, 0, 0, 0.7);
+                max-width: 660px;
+            }
+            .content{
+                padding: 20px 5px 5px 5px;
+            }
+              footer{
+                padding-top: 5px ;
+                background-color: #101010;
+                color:#9d9d9d;
+                bottom: 0;
+                width: 100%;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <nav class="navbar navbar-inverse ">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" >
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="logo img-responsive">
+                        <img src="img/TM_logo2.jpg" height="40px"  alt="true mentor">
+                    </div>
+                    <a href="#" class="navbar-brand">TRUE MENTOR </a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li> <a href="https://freecareer.herokuapp.com/"><span class="glyphicon glyphicon-pencil"></span>TAKE PSYCHOMETRIC TEST</a></li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+            <div class="jumbotron">
+            <div class=" banner_image img responsive">
+                <div class="text-justified">
+            <div class="banner_content">
+                    <h2><b> WELCOME TO TRUE MENTORS PLATFORM</b></h2><br>
+        </div>
+        </div>
+            </div>
+            </div>
+             <center>
+                <h1>HOPING THAT YOU HAVE ENJOYED THE QUIZ SESSION</h1>
+                <h2>AND FOUND THE PERFECT CARRER FOR YOU</h2>
+                <h3>HERE IS DESCRIPTION OF YOUR PREDICTED CARRER</h3>
+            </center>
+<div class="content">
+    <div class="row text-center">
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.jagranjosh.com/careers/information-technology-1528686112-1"><h3>Information Technology</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+        <a href="https://www.mindler.com/blog/civil-services-career-opportunities/"><h3>Civil Services</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://raoiit.com/what-is-neet.php"><h3>All about NEET</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+        <a href="https://raoiit.com/what-is-jee.php"><h3>All about JEE</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://raoiit.com/what-is-mbbs-bds.php"><h3>All about MBBS</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.collegedekho.com/courses/master-of-business-administration-mba/"><h3>All about MBA</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.jagranjosh.com/careers/girl-students-1528888557-1"><h3>Carrer options for women</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.apnaahangout.com/courses-to-do-after-12th-commerce/"><h3>Commerce</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://scoop.eduncle.com/how-to-become-a-professor-in-india"><h3>Professor</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.collegedekho.com/careers/teacher"><h3>Teacher</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://study.com/articles/Acting_Career_Information_Becoming_an_Actor_or_Actress.html"><h3>Acting</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.accreditedschoolsonline.org/art-schools/careers/"><h3>Arts</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.indiaeducation.net/careercenter/armed-forces/indian-army/"><h3>Defence</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.indiaeducation.net/careercenter/engineering/"><h3>Engineering</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="http://www.careerizma.com/careers/finance/"><h3>Finance</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.jagranjosh.com/careers/law-1296555438-1"><h3>Law</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.indiaeducation.net/careercenter/management/"><h3>Management</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.jagranjosh.com/articles/career-in-sales-and-marketing-1515735391-1"><h3>Sales and Marketing</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.shiksha.com/mass-communication-media/articles/careers-in-media-entertainment-industry-blogId-2260"><h3>Media and Entertainment</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.indiaeducation.net/careercenter/medical/"><h3>Medical</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://careerswithstem.com.au/science-careers-list/"><h3>Science</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.indiaeducation.net/careercenter/professional-courses/sports/"><h3>Sports</h3></a><br></div>
+        <div class="col-md-3 col-sm-6">
+            <a href="https://www.startupindia.gov.in/content/sih/en/startup-scheme.html"><h3>Start Up</h3></a><br></div>
+        
+    </div>
+</div>
+        <footer>
+            <div class="container">
+                    <ul>
+                        <a href="About_us.php">About us</a>
+                    </ul>
+                
+            </div>
+            </footer>
+    </body>
+</html>
