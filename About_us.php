e519fc6a4348a1e961fa659f368a73b5fda15068
Anuradha4781 <68979402+Anuradha4781@users.noreply.github.com>
2020-08-02 13:11:16 +0530

    Add files via upload

---
+++ About_us.php
@@ -0,0 +1,63 @@
+<html>
+    <head>
+        <title> True_Mentor.com</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-sacale=1">
+        <style>
+            .body{
+                margin:0px;
+                padding:0px;
+            }
+             .logo{
+             float: left;
+             padding-top: 5px;
+             padding-bottom: 5px;
+              }
+              .content{
+                  margin:20px;
+                  
+              }
+              </style>
+    </head>
+    <body>
+        
+       <nav class="navbar navbar-inverse ">
+            <div class="container">
+                
+                    <div class="logo img-responsive">
+                        <img src="img/TM_logo2.jpg" height="40px"  alt="true mentor">
+                    </div>
+                    <a href="index.php" class="navbar-brand">TRUE MENTOR </a>
+                </div>
+               
+            
+        </nav>
+    <center>
+        <h1><b>TRUE MENTOR</b></hi>
+    </center>
+    <div class="content">
+        <h3>
+            TRUE MENTOR is a platform to show right direction. Actually it is a solution of problem statement of <b>Government of Uttarakhand</b> to develop a <b>Carrer Counselling
+            Software.</b>To find its solution we have gone through a study and found that 42% people don't enjoy their jobs,92% don't get any carrer related guidance from schooling,
+            Parents and childrens are stressed about which carrer they should choose and if they choose any field after some time they find that it is not the field suitable for
+            them and feels uncomfortable with their work. This is the reason that the <b>Team Drifters3.0</b> from <b>Bhagalpur College Of Engineering,Bihar</b> implemented a software based on
+            a Psychometric test completely takes users interest as input and give them a suitable carrer to being comfortable in that field .We have tried to provide all essential 
+            things in one platform.<br>
+        </h3> 
+        <h2><b>How it works?</b></h2>
+        <h3>
+            Basically this software is based on <b>Artificial Intelligence</b>. You have to just register yourself and take a quiz which will be based on your interest not your knowledge.
+            The quiz has various levels like post matric,post intermediate,post graduation,proffesional, you have to choose your level also and after complemeting the quiz 1 or 2 
+            carrer will be predicted according to your choosen answer. After this you will be directed to a page that will contain the description of your predicted carrer, along
+            with it others fields description are also available if you want you can go and explore.Beside this we have provided a quiz section also where it will check your 
+            knowledge in various fields,if you want to check your knowledge then you can go and take the quiz.<br>
+        </h3>
+            
+           
+    </div>
+ </body>
+</html>
+
+
