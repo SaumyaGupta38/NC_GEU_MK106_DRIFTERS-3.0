c4fd089b7e3cea46ae8d19410addaa3ccef5ea78
Anuradha4781 <68979402+Anuradha4781@users.noreply.github.com>
2020-08-03 14:05:14 +0530

    web site1

---
+++ Civilservices/Book.php
@@ -0,0 +1,52 @@
+
+<html>
+    <head>
+        <title>Civil_Services</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-sacale=1">
+        <style>
+            body{
+                font-size: 20px;
+            }
+            table{
+    border: 1px ;
+    width:100%;
+    border-collapse: collapse;
+}
+th,td{
+    padding: 15px;
+    border:1px;
+    height:30px;
+}
+th{
+    text-align: left;
+    
+}
+        </style>
+        
+
+    <body><h3>Books You Must Follow</h3>
+<table>
+    <tbody>
+        <tr>
+            
+            <th>Subjects</th>
+            <th>Books</th>
+            
+        </tr>
+        <tr><td><h4>Geography</h4></td><td>11,12th NCERT-2 books frm each standard</td></tr>
+        <tr><td><h4>Modern Indian History</h4></td><td>Spectrum<br>Politics in india since independence-class 12th NCERT polity book</td></tr>
+        <tr><td><h4>Ancient & Mediaval History</h4></td><td>Tamilnadu Board 11th class book<br>Our Past part 1,2,3 NCERT books</td></tr>
+        <tr><td><h4>Art n Culture</h4></td><td>Nitin Singhania</td></tr>
+        <tr><td><h4>Economy</h4></td><td>Sri Ram Printed notes</td></tr>
+        <tr><td><h4>Environment</h4></td><td>Shankar IAS printed notes</td></tr>
+        <tr><td><h4>Polity</h4></td><td>Laxmikant</td></tr>
+        <tr><td><h4>World History</h4></td><td>Class 9th social science book (old NCERT)<br>Contemporary World Politics-Class 12th Polity book from NCERT</td></tr>
+        <tr><td><h4>Economic Survey and Budget Summary</h4></td><td>Summary provided by GS score</td></tr>
+        <tr><td><h4>Ethics</h4></td><td>G Subbarao<br>Current Affairs</td></tr>
+    </tbody>
+</table>
+    </body>
+</html>
